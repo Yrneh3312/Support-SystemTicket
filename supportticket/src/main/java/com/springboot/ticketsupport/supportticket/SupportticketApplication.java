6c933302d2ddd397270d54a86a1cd9c636bf0fa0
Yrneh3312 <henry.najera3312@gmail.com>
2024-03-30 15:07:03 -0600

    agregando spcd

---
+++ supportticket/src/main/java/com/springboot/ticketsupport/supportticket/SupportticketApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.ticketsupport.supportticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportticketApplication.class, args);
+	}
+
+}
