6a79877123d7cf2b0ef31f7816c1a45a83cbe33c
Yrneh3312 <henry.najera3312@gmail.com>
2024-04-07 18:20:40 -0600

    agregando

--- supportticket/src/main/java/com/springboot/ticketsupport/supportticket/models/ticket.java
+++ supportticket/src/main/java/com/springboot/ticketsupport/supportticket/models/ticket.java
@@ -1,5 +1,10 @@
 package com.springboot.ticketsupport.supportticket.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ticket")
 public class ticket {
 
 }
